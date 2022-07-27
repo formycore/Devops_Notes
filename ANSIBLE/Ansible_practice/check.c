1e2f4304779b0f8150aaeaccc19fc7e42f1d0a9b
sandeepmchary <awssandeepchary@gmail.com>
2022-07-27 17:00:26 +0530

    added notes for ansible

---
+++ ANSIBLE/Ansible_practice/check.c
@@ -0,0 +1,11 @@
+# sum of two numbers
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of a and b is %d",c);
+    return 0;
+}
