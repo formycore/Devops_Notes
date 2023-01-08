120f0b04daffcdcb44dc20368accb72947966c50
Sandeep <formycore@gmail.com>
2023-01-08 23:11:35 +0530

    added

---
+++ JENKINS/jenkins_update/jenkins_install.bat
@@ -0,0 +1,5 @@
+# install jenkins with windows batch
+# 1. download jenkins.war
+curl -o "C:\Program Files (x86)\Jenkins\jenkins.war" http://mirrors.jenkins-ci.org/war/latest/jenkins.war
+# 2. start jenkins
+net start jenkins
