850d6de6ee1a22b89237ce2d497dd231f07da0e4
Sandeep <formycore@gmail.com>
2023-04-27 19:32:57 +0530

    added

--- terraform/udemy_terraform_20_demos/6_input_datasource_output/c5ec2intance.tf
+++ terraform/udemy_terraform_20_demos/6_input_datasource_output/c5ec2intance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myec2vm" {
-  ami = data.aws_ami.centos.id
+  ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
