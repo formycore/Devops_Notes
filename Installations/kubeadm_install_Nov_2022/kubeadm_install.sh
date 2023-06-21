2b502193ec626c1b2ad4da0559b6819d400a9e62
Sandeep <formycore@gmail.com>
2023-06-21 19:20:35 +0530

    added

---
+++ Installations/kubeadm_install_Nov_2022/kubeadm_install.sh
@@ -0,0 +1,23 @@
+sudo swapoff -a
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install containerd.io -y
+sudo systemctl start containerd
+sudo systemctl enable containerd
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo yum install -y kubelet-1.24.0 kubeadm-1.24.0 kubectl-1.24.0 --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
