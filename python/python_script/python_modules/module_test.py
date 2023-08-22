716c843c4171f99ffeea6c6074636ddc54624705
Sandeep <formycore@gmail.com>
2023-08-23 00:36:36 +0530

    added

--- python/python_script/python_modules/module_test.py
+++ python/python_script/python_modules/module_test.py
@@ -1,3 +1,6 @@
 import mymodule
 
 print(mymodule.my_value)
+# these scripts should be in the same location for time being
+# here we are taking the value of my_value from the mymodule 
+# reusing the values in other scripts
