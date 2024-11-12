afafce2ab927fb9b8347446f5b20795942d3951f
Sandeep <formycore@gmail.com>
2024-11-12 21:41:22 +0530

    Add files via upload

---
+++ python/vamshi_bhavani_python/area_of_rectangle.py
@@ -0,0 +1,6 @@
+def area_of_rectangle(length, width):
+    return length * width
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+result = area_of_rectangle(length, width)
+print(f"Area of rectangle: ", result)
